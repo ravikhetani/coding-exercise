0490bf3889e0a0434cd7742c2f91f056cd388056
ravikhetani <ravi.khetani@gmail.com>
2024-11-08 10:07:21 +0000

    allow local origins

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS and allow all local origins
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        origin.startsWith('http://localhost') ||
+        origin.startsWith('http://127.0.0.1')
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
